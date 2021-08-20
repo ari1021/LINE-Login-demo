ca1ebcfec3294260d3f1d71972177ab812415e6c
ari1021 <o.pe.3.vndt.ku@gmail.com>
2021-08-20 11:00:15 +0900

    feat: add spring initializr

---
+++ src/main/kotlin/com/example/login/LineLoginDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.login
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LineLoginDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<LineLoginDemoApplication>(*args)
+}
